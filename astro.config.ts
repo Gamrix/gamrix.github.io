ebb694a830a88b44164cd4d82b53b997da29c292
John Clow <johnwclow@hotmail.com>
2025-10-04 14:32:32 -0700

    Template version that is works with github actions

--- astro.config.ts
+++ astro.config.ts
@@ -3,11 +3,13 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, fontProviders } from "astro/config";
 
+const githubPagesUrl = "https://gamrix.github.io";
+
 const site = process.env.VERCEL
   ? process.env.VERCEL_ENV === "production"
     ? "https://astro-shadcn-ui-template.vercel.app"
     : `https://${process.env.VERCEL_URL}`
-  : (process.env.SITE ?? "http://localhost:4321");
+  : (process.env.SITE || githubPagesUrl);
 const base = process.env.BASE || "/";
 
 // https://astro.build/config
